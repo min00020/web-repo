0861d28e2079d89c572a5f9eef66d52c15bf3d68
SubPC <SubPC@192.168.0.103>
2023-10-27 08:15:36 +0900

    23.10.26 object,string; 예제문제2

--- helloWeb/src/main/webapp/js/string2.js
+++ helloWeb/src/main/webapp/js/string2.js
@@ -4,7 +4,7 @@ const words = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit i
 
 
 //[1] 공백을 기준으로 단어를 가져와서 가져온 단어의 크기가 (length) 5보다 큰 단어를 콘솔출력
-let result = words.split(" ");
+let result = words.replace(/\,/g, '').replace(/\./g, '').split(" ");
 //words.match(/\w+/gi);
 
 for (let i=0; i<result.length; i++){
@@ -25,11 +25,11 @@ function checkGender(ssn){
 		return '여자';
 	}
 }
-console.log("<성별 구분>--------------")
+console.log("<성별 구분>--------------");
 console.log('	성별:', checkGender('930307232221'));
 console.log('	성별:', checkGender('930305-4432221'));
 console.log('	성별:', checkGender('930305 1432221'));
-console.log("------------------------")
+console.log("------------------------");
 
 
 //[3] 파일명과 파일의 확장자
@@ -43,17 +43,7 @@ let fileLast = result[result.length -1].split(".")
 fileName = fileLast[0];
 fileExt = fileLast[1];
 
-console.log('파일명: ',fileName);
-console.log('확장자: ',fileExt);
-
-
-
-
-
-
-const words2 = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iusto animi labore voluptatum corporis laborum deserunt ex, soluta ut commodi veniam dolore suscipit voluptas magnam quam quas repellendus. Nulla, dolorib';
-
-
-console.log (words.replace(',',''));
+console.log('파일명:',fileName);
+console.log('확장자:',fileExt);
 
 
--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -1,52 +1,47 @@
 //calendar.js		
-		
-		const today = new Date();
-		console.log("오늘날짜:"+today.getDate());
-		
-		function makeHead(){
-		const days = ['일','월','화','수','목','금','토']
-			return days.reduce((acc,day) => {
-				return acc + '<th>'+day +'</th>';
-			},'<thead><tr>')
+
+const today = new Date();
+console.log("오늘날짜:" + today.getDate());
+
+function makeHead() {
+	const days = ['일', '월', '화', '수', '목', '금', '토']
+	return days.reduce((acc, day) => {
+		return acc + '<th>' + day + '</th>';
+	}, '<thead><tr>')
+}
+
+function makeBody() {
+	let styles = '';
+	let tr = '</tr></thead><tbody>';
+	for (let i = 1; i <= 31; i++) {
+		if (i == today.getDate()) {
+			styles = 'font-weight:bolder;';
 		}
-		
-		function makeBody(){
-			let styles='';
-			let tr ='</tr></thead><tbody>';
-			for(let i=1; i<=31; i++){
-				//오늘날짜는 백그라운드:노란색, 폰트:bold;
-				if(i % 7 == 0){
-					if(i == today.getDate()){
-						styles='font-weight:bolder;'
-					}
-					tr += '<td style ="'+styles+'color:blue; background-color:lightgray", align = "right">'+i+'</td></tr><tr>';
-				}
-				else if(i%7 == 1){
-					if(i == today.getDate()){
-						styles='font-weight:bolder;'
-					}
-					tr += '<td style ="'+styles+'color:red;background-color:lightpink;", align = "right">'+i+'</td>'
-				}
-				else {
-					if(i == today.getDate()){
-						styles='font-weight:bolder;'
-					}
-					tr += '<td style ="'+styles+'", align = "right">'+i+'</td>'
-				}
-				
-			}//end for
-			tr += '</tr></tbody>';
-			return tr;
+		else if (styles != '' && i != today.getDate()) {
+			styles = '';
 		}
-		console.log(makeBody());
-		
-		function makeCalendar(){
-			let table = "";
-			table += '<table border="1">';
-			table += makeHead();
-			table += makeBody();
-			table += '</table>';
-			let show = document.getElementById('show').innerHTML = table;
-			return show;
+		if (i % 7 == 0) {
+			tr += '<td style ="' + styles + 'color:blue; background-color:lightgray", align = "right">' + i + '</td></tr><tr>';
 		}
-		makeCalendar();
+		else if (i % 7 == 1) {
+			tr += '<td style ="' + styles + 'color:red;background-color:lightpink;", align = "right">' + i + '</td>'
+		}
+		else {
+			tr += '<td style ="' + styles + '", align = "right">' + i + '</td>'
+		}
+	}//end for
+	tr += '</tr></tbody>';
+	return tr;
+}
+console.log(makeBody());
+
+function makeCalendar() {
+	let table = "";
+	table += '<table border="1">';
+	table += makeHead();
+	table += makeBody();
+	table += '</table>';
+	let show = document.getElementById('show').innerHTML = table;
+	return show;
+}
+makeCalendar();
